a7772613ef9e6fdfddb1368db7e3ebec239144db
Alex Wayland <89842550+web-lynx@users.noreply.github.com>
2022-02-09 13:42:08 +0000

    Changed to one CSS file and updated CSS significantly to make it less ugly

--- multiplayer.js
+++ multiplayer.js
@@ -35,162 +35,162 @@ let playerTwoLosses = 0;
 // Game function - Player 1
 
 let playerOneStart = () => {
-    player1Roll.textContent = "Roll the die"
-    let currentRoll = Math.ceil(Math.random() * 6);
-    dieImage.src = `./images/${currentRoll}.png`;
-    dieImage.style =`visibility: visible`;
-    playerOneScore += currentRoll;
-    player1Score.textContent = `Score: ${playerOneScore}`;
+  player1Roll.textContent = "Roll the die";
+  let currentRoll = Math.ceil(Math.random() * 6);
+  dieImage.src = `./images/${currentRoll}.png`;
+  dieImage.style = `visibility: visible`;
+  playerOneScore += currentRoll;
+  player1Score.textContent = `Score: ${playerOneScore}`;
 
-    if(playerOneWins > 2) {
-        gameBanner.textContent = "PLAYER ONE WINS THE MATCH! CONGRATULATIONS!";
-        player2Roll.disabled = true;
-        player2Pass.disabled = true;
-        player1Roll.disabled = true;
-        player1Pass.disabled = true;
-        replay.style =`visibility: visible`;
-    }
+  if (playerOneWins > 2) {
+    gameBanner.textContent = "PLAYER ONE WINS THE MATCH! CONGRATULATIONS!";
+    player2Roll.disabled = true;
+    player2Pass.disabled = true;
+    player1Roll.disabled = true;
+    player1Pass.disabled = true;
+    replay.style = `visibility: visible`;
+  }
 
-    if(playerOneLosses > 2) {
-        gameBanner.textContent = "PLAYER TWO WINS BY DEFAULT! BAD LUCK PLAYER ONE!";
-        player2Roll.disabled = true;
-        player2Pass.disabled = true;
-        player1Roll.disabled = true;
-        player1Pass.disabled = true;
-        replay.style =`visibility: visible`;
-    }
+  if (playerOneLosses > 2) {
+    gameBanner.textContent = "PLAYER TWO WINS BY DEFAULT! BAD LUCK PLAYER ONE!";
+    player2Roll.disabled = true;
+    player2Pass.disabled = true;
+    player1Roll.disabled = true;
+    player1Pass.disabled = true;
+    replay.style = `visibility: visible`;
+  }
 
-    if(playerTwoWins > 2) {
-        gameBanner.textContent = "PLAYER TWO WINS THE MATCH! CONGRATULATIONS!";
-        player2Roll.disabled = true;
-        player2Pass.disabled = true;
-        player1Roll.disabled = true;
-        player1Pass.disabled = true;
-        replay.style =`visibility: visible`;
-    }
+  if (playerTwoWins > 2) {
+    gameBanner.textContent = "PLAYER TWO WINS THE MATCH! CONGRATULATIONS!";
+    player2Roll.disabled = true;
+    player2Pass.disabled = true;
+    player1Roll.disabled = true;
+    player1Pass.disabled = true;
+    replay.style = `visibility: visible`;
+  }
 
-    if(playerTwoLosses > 2) {
-        gameBanner.textContent = "PLAYER ONE WINS BY DEFAULT! BAD LUCK PLAYER TWO!";
-        player2Roll.disabled = true;
-        player2Pass.disabled = true;
-        player1Roll.disabled = true;
-        player1Pass.disabled = true;
-        replay.style =`visibility: visible`;
-    }
-    
-    if (playerOneScore >= 21) {
-        player1Score.textContent = `You've won with a score of ${[playerOneScore]}`;
-        playerOneScore = 0;
-        playerOneWins ++;
-        player1Wins.textContent = `Wins: ${playerTwoWins}`
-        player1Roll.textContent = "Play again?"
-        player2Roll.disabled = false;
-        player2Pass.disabled = false;
-        player1Roll.disabled = true;
-        player1Pass.disabled = true;
-    }
-    
-    if (currentRoll == 1) {
-        player1Score.textContent = `You lost`;
-        playerOneScore = 0;
-        playerOneLosses ++;
-        player1Losses.textContent = `Losses: ${playerOneLosses}`
-        player1Roll.textContent = "Play again?"
-        player2Roll.disabled = false;
-        player2Pass.disabled = false;
-        player1Roll.disabled = true;
-        player1Pass.disabled = true;
-    }
-}
+  if (playerTwoLosses > 2) {
+    gameBanner.textContent = "PLAYER ONE WINS BY DEFAULT! BAD LUCK PLAYER TWO!";
+    player2Roll.disabled = true;
+    player2Pass.disabled = true;
+    player1Roll.disabled = true;
+    player1Pass.disabled = true;
+    replay.style = `visibility: visible`;
+  }
 
-// Game function - Player 2
-
-let playerTwoStart = () => {
-    player2Roll.textContent = "Roll the die"
-    let currentRoll = Math.ceil(Math.random() * 6);
-    dieImage.src = `./images/${currentRoll}.png`;
-    dieImage.style =`visibility: visible`;
-    playerTwoScore += currentRoll;
-    player2Score.textContent = `Score: ${playerTwoScore}`;
-    
-    if(playerTwoWins > 2) {
-        gameBanner.textContent = "PLAYER TWO WINS THE MATCH! CONGRATULATIONS!";
-        player2Roll.disabled = true;
-        player2Pass.disabled = true;
-        player1Roll.disabled = true;
-        player1Pass.disabled = true;
-        replay.style =`visibility: visible`;
-    }
-
-    if(playerTwoLosses > 2) {
-        gameBanner.textContent = "PLAYER ONE WINS BY DEFAULT! BAD LUCK PLAYER TWO!";
-        player2Roll.disabled = true;
-        player2Pass.disabled = true;
-        player1Roll.disabled = true;
-        player1Pass.disabled = true;
-        replay.style =`visibility: visible`;
-    }
-
-    if(playerOneWins > 2) {
-        gameBanner.textContent = "PLAYER ONE WINS THE MATCH! CONGRATULATIONS!";
-        player2Roll.disabled = true;
-        player2Pass.disabled = true;
-        player1Roll.disabled = true;
-        player1Pass.disabled = true;
-        replay.style =`visibility: visible`;
-    }
-
-    if(playerOneLosses > 2) {
-        gameBanner.textContent = "PLAYER TWO WINS BY DEFAULT! BAD LUCK PLAYER ONE!";
-        player2Roll.disabled = true;
-        player2Pass.disabled = true;
-        player1Roll.disabled = true;
-        player1Pass.disabled = true;
-        replay.style =`visibility: visible`;
-    }
-
-    if (playerTwoScore >= 21) {
-        player2Score.textContent = `You've won with a score of ${[playerTwoScore]}`;
-        playerTwoScore = 0;
-        playerTwoWins ++;
-        player2Wins.textContent = `Wins: ${playerTwoWins}`
-        player2Roll.textContent = "Play again?"
-        player2Roll.disabled = true;
-        player2Pass.disabled = true;
-        player1Roll.disabled = false;
-        player1Pass.disabled = false;
-    }
-    
-    if (currentRoll == 1) {
-        player2Score.textContent = `You lost`;
-        playerTwoScore = 0;
-        playerTwoLosses ++;
-        player2Losses.textContent = `Losses: ${playerTwoLosses}`
-        player2Roll.textContent = "Play again?"
-        player2Roll.disabled = true;
-        player2Pass.disabled = true;
-        player1Roll.disabled = false;
-        player1Pass.disabled = false;
-    }
-}
-
-//Pass function - Player 1
-
-let playerOnePass = () => {
+  if (playerOneScore >= 21) {
+    player1Score.textContent = `You've won with a score of ${[playerOneScore]}`;
+    playerOneScore = 0;
+    playerOneWins++;
+    player1Wins.textContent = `Wins: ${playerTwoWins}`;
+    player1Roll.textContent = "Play again?";
     player2Roll.disabled = false;
     player2Pass.disabled = false;
     player1Roll.disabled = true;
     player1Pass.disabled = true;
-}
+  }
 
-//Pass function - Player 2
+  if (currentRoll == 1) {
+    player1Score.textContent = `You lost`;
+    playerOneScore = 0;
+    playerOneLosses++;
+    player1Losses.textContent = `Losses: ${playerOneLosses}`;
+    player1Roll.textContent = "Play again?";
+    player2Roll.disabled = false;
+    player2Pass.disabled = false;
+    player1Roll.disabled = true;
+    player1Pass.disabled = true;
+  }
+};
 
-let playerTwoPass = () => {
+// Game function - Player 2
+
+let playerTwoStart = () => {
+  player2Roll.textContent = "Roll the die";
+  let currentRoll = Math.ceil(Math.random() * 6);
+  dieImage.src = `./images/${currentRoll}.png`;
+  dieImage.style = `visibility: visible`;
+  playerTwoScore += currentRoll;
+  player2Score.textContent = `Score: ${playerTwoScore}`;
+
+  if (playerTwoWins > 2) {
+    gameBanner.textContent = "PLAYER TWO WINS THE MATCH! CONGRATULATIONS!";
+    player2Roll.disabled = true;
+    player2Pass.disabled = true;
+    player1Roll.disabled = true;
+    player1Pass.disabled = true;
+    replay.style = `visibility: visible`;
+  }
+
+  if (playerTwoLosses > 2) {
+    gameBanner.textContent = "PLAYER ONE WINS BY DEFAULT! BAD LUCK PLAYER TWO!";
+    player2Roll.disabled = true;
+    player2Pass.disabled = true;
+    player1Roll.disabled = true;
+    player1Pass.disabled = true;
+    replay.style = `visibility: visible`;
+  }
+
+  if (playerOneWins > 2) {
+    gameBanner.textContent = "PLAYER ONE WINS THE MATCH! CONGRATULATIONS!";
+    player2Roll.disabled = true;
+    player2Pass.disabled = true;
+    player1Roll.disabled = true;
+    player1Pass.disabled = true;
+    replay.style = `visibility: visible`;
+  }
+
+  if (playerOneLosses > 2) {
+    gameBanner.textContent = "PLAYER TWO WINS BY DEFAULT! BAD LUCK PLAYER ONE!";
+    player2Roll.disabled = true;
+    player2Pass.disabled = true;
+    player1Roll.disabled = true;
+    player1Pass.disabled = true;
+    replay.style = `visibility: visible`;
+  }
+
+  if (playerTwoScore >= 21) {
+    player2Score.textContent = `You've won with a score of ${[playerTwoScore]}`;
+    playerTwoScore = 0;
+    playerTwoWins++;
+    player2Wins.textContent = `Wins: ${playerTwoWins}`;
+    player2Roll.textContent = "Play again?";
     player2Roll.disabled = true;
     player2Pass.disabled = true;
     player1Roll.disabled = false;
     player1Pass.disabled = false;
-}
+  }
+
+  if (currentRoll == 1) {
+    player2Score.textContent = `You lost`;
+    playerTwoScore = 0;
+    playerTwoLosses++;
+    player2Losses.textContent = `Losses: ${playerTwoLosses}`;
+    player2Roll.textContent = "Play again?";
+    player2Roll.disabled = true;
+    player2Pass.disabled = true;
+    player1Roll.disabled = false;
+    player1Pass.disabled = false;
+  }
+};
+
+//Pass function - Player 1
+
+let playerOnePass = () => {
+  player2Roll.disabled = false;
+  player2Pass.disabled = false;
+  player1Roll.disabled = true;
+  player1Pass.disabled = true;
+};
+
+//Pass function - Player 2
+
+let playerTwoPass = () => {
+  player2Roll.disabled = true;
+  player2Pass.disabled = true;
+  player1Roll.disabled = false;
+  player1Pass.disabled = false;
+};
 
 // Event Listeners
 
@@ -202,4 +202,4 @@ player1Pass.addEventListener("click", playerOnePass);
 
 player2Pass.addEventListener("click", playerTwoPass);
 
-replay.addEventListener("click", () => window.location.reload())
+replay.addEventListener("click", () => window.location.reload());
--- singleplayer.js
+++ singleplayer.js
@@ -4,22 +4,22 @@ const diceImage = document.getElementById("diceImage");
 let player1Score = 0;
 
 let startSinglePlayer = () => {
-    rollButton.textContent = "Roll the die"
-    let currentRoll = Math.ceil(Math.random() * 6);
-    diceImage.src = `./images/${currentRoll}.png`;
-    diceImage.style =`visibility: visible`;
-    player1Score += currentRoll;
-    score.textContent = `Score: ${player1Score}`;
-    if (player1Score >= 21) {
-        score.textContent = `You've won with a score of ${[player1Score]}`;
-        player1Score = 0;
-        rollButton.textContent = "Play again?"
-        }
-    if (currentRoll == 1) {
-            score.textContent = `You lost`;
-            player1Score = 0;
-            rollButton.textContent = "Play again?"
-        }
-    }
+  rollButton.textContent = "Roll the die";
+  let currentRoll = Math.ceil(Math.random() * 6);
+  diceImage.src = `./images/${currentRoll}.png`;
+  diceImage.style = `visibility: visible`;
+  player1Score += currentRoll;
+  score.textContent = `Score: ${player1Score}`;
+  if (player1Score >= 21) {
+    score.textContent = `You've won with a score of ${[player1Score]}`;
+    player1Score = 0;
+    rollButton.textContent = "Play again?";
+  }
+  if (currentRoll == 1) {
+    score.textContent = `You lost`;
+    player1Score = 0;
+    rollButton.textContent = "Play again?";
+  }
+};
 
 rollButton.addEventListener("click", startSinglePlayer);
